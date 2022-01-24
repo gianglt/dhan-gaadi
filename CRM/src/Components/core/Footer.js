c0b3696680b1708fbc8dd9d0b0aaddf06cac9b69
Giang <gianglt@gmail.com>
2022-01-24 16:19:44 +0700

    Chạy

--- CRM/src/Components/core/Footer.js
+++ CRM/src/Components/core/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
 			<div className="row">
 				<div>
 					<div className="description-block border-right">
-						<h5 className="description-header">&copy; 2020 Dhan-gaadi</h5>
+						<h5 className="description-header">&copy; 2022 ICCO</h5>
 					</div>
 				</div>
 			</div>
--- server/helpers/dbConnection.js
+++ server/helpers/dbConnection.js
@@ -4,13 +4,14 @@ module.exports = () => {
     const self = module.exports;
     return mongoose
         .connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useCreateIndex: true
+            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useCreateIndex: true
         })
         .then(() => console.log("DB Connected"))
         .catch(err => {
             console.error(
-                "Failed to connect to mongo on startup - retrying in 5 sec"
+                "Failed to connect to mongo on startup - retrying in 5 sec. ERROR: " + err.message
             );
             setTimeout(self, 5000);
         });
