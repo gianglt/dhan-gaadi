b1e4e746cd57c5a8d1f4b231199246a9927e18c3
Giang <gianglt@gmail.com>
2022-01-25 16:42:59 +0700

    111

--- server/helpers/dbConnection.js
+++ server/helpers/dbConnection.js
@@ -5,8 +5,8 @@ module.exports = () => {
     return mongoose
         .connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
-            // useNewUrlParser: true,
-            // useCreateIndex: true
+            useNewUrlParser: true,
+            useCreateIndex: true
         })
         .then(() => console.log("DB Connected"))
         .catch(err => {
